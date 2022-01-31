13909bda07e58e20e66293ebd8e3304474e85d7c
bybang <bybang26@gmail.com>
2022-01-31 14:19:25 -0500

    Implement game logic, start working on main.rb

--- game.rb
+++ game.rb
@@ -1,8 +1,19 @@
 class Game
   attr_accessor :current_player, :next_player
-  def initialize(current_player, next_player)
-    @current_player = current_player
-    @next_player = next_player
+  def initialize(player1, player2)
+    @current_player = player1
+    @next_player = player2
   end
 
+  def generate_quiz
+    num1 = rand(1...20)
+    num2 = rand(1...20)
+
+    puts "#{current_player}: What does #{num1} plus #{num2} equals?"
+    print "> "
+
+    user_answer = $stdin.gets.chomp.to_i
+    answer = num1 + num2
+    user_answer == answer
+  end
 end
--- main.rb
+++ main.rb
@@ -1,17 +1,9 @@
 require './game.rb'
 require './player.rb'
 
-def start
-  puts "It's #{}'s turn"
-  # player{x}: what does 5 plus 3 eqaul?
-  print "> "
-  choice = $stdin.gets.chomp
-
-  if choice == "correct"
-    bear_room
-  elsif choice == "wrong"
-    cthulhu_room
-  else
-    dead("You stumble around the room until you starve.")
-  end
+def game_start
+  puts "Welcome to TwO-O-Player Math Game!"
+  print "Enter your name: "
+  player1 = Player.new(gets.chomp)
+  
 end
